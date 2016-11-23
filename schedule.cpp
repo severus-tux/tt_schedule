888fabb059d35e12ec416d30fce09dcb22d7fbbd
Vishwa Prakash H V <vishwaprakash@protonmail.com>
2016-11-23 17:50:22 +0530

    updated schedule.cpp and schedule.h

--- schedule.cpp
+++ schedule.cpp
@@ -4,3 +4,30 @@ schedule::schedule()
 {
 
 }
+
+void schedule::schedule_hrd_math()
+{
+    db_shdl = new tt_database();
+    db_shdl->connOpen();
+    QSqlQuery qry_hrd_math;
+    qry_hrd_math.prepare("update master  "
+                    "set sub_id = 'HRD' "
+                    "where exists ( "
+                    "   select * from hrd "
+                    "   where hrd.day=master.day and "
+                    "   hrd.sem=master.sem and "
+                    "   hrd.sec=master.sec and "
+                    "   (hrd.hour=master.hour or hrd.hour=master.hour-1) )");
+    qry_hrd_math.exec();
+
+    qry_hrd_math.prepare("update master  "
+                    "set sub_id = ( select sub_id from subject where sub_name='MATHS-3')) "
+                    "where exists ( "
+                    "   select * from maths "
+                    "   where maths.day=master.day and "
+                    "   maths.sem=master.sem and "
+                    "   maths.sec=master.sec and "
+                    "   maths.hour=master.hour )");
+    qry_hrd_math.exec();
+    db_shdl->connClose();
+}
--- schedule.h
+++ schedule.h
@@ -1,11 +1,16 @@
 #ifndef SCHEDULE_H
 #define SCHEDULE_H
 
+#include "tt_database.h"
+#include <QSqlQuery>
 
 class schedule
 {
 public:
     schedule();
+    void schedule_hrd_math();
+private:
+    tt_database *db_shdl;
 };
 
 class teacher:public schedule
