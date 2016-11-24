8bcc2b7a1ffc4217e53ed70bb4d8b99c82c92a2e
Shreyas P V <shreyaspv25@gmail.com>
2016-11-24 11:02:49 +0530

    added initialize_master function

--- schedule.cpp
+++ schedule.cpp
@@ -31,3 +31,47 @@ void schedule::schedule_hrd_math()
     qry_hrd_math.exec();
     db_shdl->connClose();
 }
+void schedule::initialize_master()
+{
+    db_shdl = new tt_database();
+    db_shdl->connOpen();
+
+    //initializing master with default values
+    QStringList days,hours;
+    days << "MON" << "TUE" << "WED" << "THU" << "FRI" << "SAT";
+    hours << "1" << "2" << "3" << "4" << "5" << "6" << "7"; //hours in string list beacuse of error cannot
+                                                            //convert int to char while preparing query
+    QSqlQuery qry("select * from class;");
+    QVector<QString> sem_sec;
+    while(qry.next())
+    {
+        QString tmp;
+        QSqlRecord record = qry.record();
+        for (int i=0;i<record.count();i++)
+        {
+            tmp.append(record.value(i).toString());
+            tmp.append(",'");
+        }
+        tmp.chop(2);
+        sem_sec.append(tmp);
+    }
+
+    for(int day = 0 ; day < 6 ; day++) //days
+    {
+        for(int i = 0 ; i < sem_sec.length() ; i++) //for existing sem and sec
+        {
+            for(int hour = 0 ; hour < 7 ; hour++) //hours
+            {
+                QString insert_qry;
+                insert_qry = "INSERT INTO MASTER VALUES ('"+days.at(day)+"',"+sem_sec.at(i)+"',"+hours.at(hour)+",NULL)";
+                QSqlQuery final_qry;
+                final_qry.exec(insert_qry);
+                //qDebug() << insert_qry;
+            }
+
+        }
+
+    }
+
+    db_shdl->connClose();
+}
--- schedule.h
+++ schedule.h
@@ -9,6 +9,7 @@ class schedule
 public:
     schedule();
     void schedule_hrd_math();
+    void initialize_master();
 private:
     tt_database *db_shdl;
 };
